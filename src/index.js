e1092e725f837c078af687ca064d907fe0bcf689
Ollie Holden <odholden@gmail.com>
2020-05-13 12:48:28 +0100

    added request methods to cors

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
