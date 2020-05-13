7c0217d32c92e359d647d788ebaebd5ef4414422
Ollie Holden <odholden@gmail.com>
2020-05-13 11:16:53 +0100

    added cors fix

--- src/index.js
+++ src/index.js
@@ -16,6 +16,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api/recipes', recipesRoutes);
 app.use('/api/favourites', favouritesRoutes);
 app.get('/api', (req, res) => res.send({ message: "Welcome to the MealWorm API!" }));
